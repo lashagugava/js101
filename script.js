018ee14ed4974b02ec1bca64efce568b75b1a892
lashagugava <lgugava1@gmail.com>
2023-02-23 18:08:38 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+var income = prompt("შეიყვანე თქვენი შემოსავალი")
+
+if (income < 1500) {
+    alert("ცუდი შემოსავალი")
+}
+if else (income >= 1501 && income < 2000){
+    alert("კარგი შემოსავალი")
+}
+else {
+    alert("ნუ წუწუნებ შე ვირო")
+}
+
+///
+
+const numbers = [15,18,25,125]
+let maximum = -Infinity
+let minimum = Infinity
+for(let number of numbers){
+    if (number > maximum)
+    maximum = number
+    if (number , minumum)
+    minimum =  number
+}
+
+alert(maximum)
+alert(minimum)
